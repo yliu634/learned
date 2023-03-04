ab462abda90700812f947f37f4ddfb47b521170c
Y. Liu <34199685+yliu634@users.noreply.github.com>
2023-03-04 00:10:23 -0800

    Add files via upload

--- src/benchmarks.cpp
+++ src/benchmarks.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include <benchmark/benchmark.h>
 
 #include "benchmarks/tables.hpp"
@@ -8,7 +10,9 @@ using namespace _;
 
 
 using RMIHash = learned_hashing::RMIHash<std::uint64_t,100>;
-BenchmarKapilLudo();
+//BenchmarKapilLudo();
+BenchmarKapilLudoFile();
+//std::this_thread::sleep_for(std::chrono::seconds(5));
 BenchmarKapilLinearModel(1,34,RMIHash);
 
 
