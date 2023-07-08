8a662ea2c1a6ceab492275b1e01811722691edbc
yliu634 <yliu634@ucsc.edu>
2023-07-08 14:25:34 -0700

    old parrot

--- include/parrot.hpp
+++ include/parrot.hpp
@@ -13,7 +13,6 @@
 #include <cstdint>
 #include <cstring>
 #include <ctime>
-#include <queue>
 #include <learned_hashing.hpp>
 #include <absl/hash/hash.h>
 
@@ -69,8 +68,6 @@ public:
   // NOTE:  0 < kLoadFactor <= 1.0
   static constexpr double kLoadFactor = 0.90;
   static constexpr K Sentinel = std::numeric_limits<K>::max();
-  static constexpr uint32_t Sentinel32 = std::numeric_limits<uint32_t>::max();
-  static constexpr uint32_t Sentinel32Mask = (1UL << 31) - 1;
   // Cuckoo insert:  The maximum number of entries to scan should be ~400
   // (Source:  Personal communication with Michael Mitzenmacher;  empirical
   // experiments validate.).  After trying 400 candidate locations, declare
@@ -81,7 +78,7 @@ public:
   static constexpr uint8_t kMaxBFSPathLen = 5;
   
   static const uint8_t kSlotsPerBucket = 4;   // modification to this value leads to undefined behavior
-  static const uint8_t kSlotPerLinknode = 4;
+  static const uint8_t kSlotPerLinknode = 2;
     
   uint32_t num_buckets_;
   AbslFastHasherTuple<K> h;
@@ -119,73 +116,26 @@ public:
   using ParrotCommon<K, V, VL>::Sentinel;
   
   struct LinkNode {
-    uint8_t seed = 0;
-    uint8_t occupiedMask = 0;
-    K bar;
+    //K keys[kSlotsPerBucket];
+    //V values[kSlotsPerBucket];
     std::array<K, kSlotPerLinknode> keys;
     std::array<V, kSlotPerLinknode> values;
     //uint8_t fingerprints[kSlotsPerBucket];
     LinkNode* next = nullptr;
+    
     LinkNode() {
-      bar = Sentinel;
       std::fill(keys.begin(), keys.end(), Sentinel);
     }
     void insert(const K& key, const V& value, support::Tape<LinkNode>& tape) {
       LinkNode* previous = this;
-      int8_t target_slot = -1;
       for (LinkNode* current = previous; current != nullptr; current = current->next) {
-        for (size_t slot = 0; slot < kSlotPerLinknode; slot++) {
-          if (occupiedMask & (1 << slot)) 
-            continue;
-          else if (target_slot == -1) {
-            target_slot = slot;
-            break;
+        for (size_t i = 0; i < kSlotPerLinknode; i++) {
+          if (current->keys[i] == Sentinel) {
+            current->keys[i] = key;
+            current->values[i] = value;
+            return;
           }
         }
-        //std::cout << "current bar is: " << current->bar << std::endl;
-        if (target_slot != -1) {
-          if (key < current->bar) current->bar = key; // update bar
-          current->keys[target_slot] = key;
-          current->values[target_slot] = value;
-          current->occupiedMask |= 1U << target_slot;
-
-          AbslFastHasherTuple<K> h;
-          bool occupied[4];
-          for (uint8_t iSeed = 0; iSeed < 255; ++iSeed) {
-            *(uint32_t *) occupied = 0U;
-            h.setSeed(iSeed);
-            //std::cout << "seed: " << unsigned(seed) << std::endl;
-            bool success = true;
-            for (char slot = 0; slot < kSlotPerLinknode; ++slot) {
-              if (current->occupiedMask & (1 << slot)) {
-                uint8_t i = uint8_t(h(current->keys[slot]) >> 6);
-                if (occupied[i]) {
-                  success = false;
-                  break;
-                } else { occupied[i] = true; }
-              }
-            }
-
-            if (success) {
-              LinkNode cpLinkNode = *(current); 
-              current->occupiedMask = 0U;
-              current->seed = iSeed;
-              h.setSeed(iSeed);
-              for (char slot = 0; slot < kSlotPerLinknode; ++slot) {
-                if (cpLinkNode.occupiedMask & (1 << slot)) {
-                  K moveKey = cpLinkNode.keys[slot];
-                  uint8_t toSlot = uint8_t(h(moveKey) >> 6);
-                  current->occupiedMask |= 1U << toSlot;
-                  current->keys[toSlot] = moveKey;
-                  current->values[toSlot] = cpLinkNode.values[slot];
-                }
-              }
-              return;
-            }
-          }
-          throw std::runtime_error("more than 255 times in separating keys in linknode");
-          return;
-        } 
         previous = current;
       }
       previous->next = tape.alloc();
@@ -198,7 +148,6 @@ public:
   struct Bucket {
     uint8_t seed = 0;
     uint8_t occupiedMask = 0;
-    K bar = Sentinel;
     K keys[kSlotsPerBucket];
     V values[kSlotsPerBucket];
     LinkNode* head = nullptr;
@@ -209,6 +158,7 @@ public:
       }
       return false;
     }
+
     inline bool insertLinkNode(const K &k, const V &v, support::Tape<LinkNode>& tape) {
       if (!head) {
         LinkNode* phead = new LinkNode();
@@ -264,8 +214,8 @@ public:
 
     // obtain list of keys -> necessary for model training
     std::vector<K> keys;
-    keys.reserve(kv.size());
-    std::transform(kv.begin(), kv.end(), std::back_inserter(keys),
+    keys.reserve(kv.size()-1);
+    std::transform(kv.begin()+1, kv.end(), std::back_inserter(keys),
                    [](const auto& p) { return p.first; });
 
     // train model on sorted data
@@ -278,7 +228,7 @@ public:
       for (int i = 0; i < toInsert; ++i) {
         insert(keys[i], i, false);
       }
-    }
+    } 
   }
   
   ParrotControlPlane(const vector<K> &keys, 
@@ -303,6 +253,21 @@ public:
       }
     }
   }
+
+  pair<uint32_t, uint32_t> locate(const K &k) const {
+    uint32_t buckets[2];
+    fast_map_to_buckets(h(k), buckets, buckets_.size());
+    
+    for (uint32_t &b : buckets) {
+      const Bucket &bucket = buckets_[b];
+      for (uint32_t slot = 0; slot < kSlotsPerBucket; slot++) {
+        if ((bucket.occupiedMask & (1 << slot)) && (bucket.keys[slot] == k)) {
+          return make_pair(b, slot);
+        }
+      }
+    }
+    return make_pair((uint32_t) -1, (uint32_t) -1);
+  }
   
   inline uint32_t size() const {
     return nKeys;
@@ -319,6 +284,7 @@ public:
     } else {
       int8_t target_bucket = -1;
       char target_slot = -1;
+
       for (char slot = 0; slot < kSlotsPerBucket; slot++) {
         if (bucket.occupiedMask & (1 << slot)) {
             continue;
@@ -333,9 +299,8 @@ public:
       bool debug(false);
       if (target_slot != -1) {
         // if(full_debug) Clocker::count("direct insert");
-        bucket.bar = k < bucket.bar? k:bucket.bar;
         succ = putItem(k, v, bucketId, target_slot);
-        if (!succ) result = -2;   //did not find proper seed to seperate them
+        if (!succ) result = -2; // did not find proper seed to seperate them
         if (debug) std::cout << "insert in bucket: " << bucketId  << " result: " << result << std::endl;
       } else {
         // if(full_debug) Clocker::count("overflow link insert");
@@ -348,7 +313,7 @@ public:
       V _;
       auto ret = lookUp(k, _);
       if (_ != v) {
-        std::cout << "k: " << k  << " v: " << _ << std::endl;
+        std::cout << "k: " << k << std::endl;
         throw std::runtime_error("wrong thing happend!!");
       }
 
@@ -405,7 +370,7 @@ public:
     uint32_t bucketId = model(k) % buckets_.size();
     const Bucket &bucket = buckets_[bucketId];
 
-    if (FindInBucket2(k, bucket, out)) { 
+    if (FindInBucket(k, bucket, out)) { 
       return true;
     } 
     // there is sth wrong
@@ -428,38 +393,19 @@ public:
       }
     }
     for (auto phead = bucket.head; phead != nullptr; phead = phead->next) {
-      if (phead->bar > k) return true;
-      h.setSeed(phead->seed);
       for (size_t i = 0; i < kSlotPerLinknode; i++) {
         if (phead->keys[i] == k) {
           out = phead->values[i];
           return true;
-        } 
+        } else if (phead->keys[i] == Sentinel) {
+          return false;
+        }
       }
     }
     // std::cout << "found nothing" << std::endl;
     return false;
   }
   
-  inline bool FindInBucket2(const K &k, const Bucket &bucket, V &out) const {
-    AbslFastHasherTuple<K> h;
-    h.setSeed(bucket.seed);
-    /*for (char s = 0; s < kSlotsPerBucket; s++) {
-      if ((bucket.occupiedMask & (1U << s)) && (bucket.keys[s] == k)) {
-        out = bucket.values[s];
-        return true;
-      }
-    }*/
-    
-    out = bucket.values[h(k) >> 6];
-    for (auto phead = bucket.head; phead != nullptr; phead = phead->next) {
-      if (phead->bar > k) return true;
-      h.setSeed(phead->seed);
-      out = phead->values[h(k) >> 6];
-    }
-    // std::cout << "found nothing" << std::endl;
-    return false;
-  }
     
   // For the associative cuckoo table, check all of the slots in
   // the bucket to see if the key is present.
@@ -529,10 +475,12 @@ public:
         return seed;
       }
     }
+    
     throw runtime_error("Cannot generate a proper hash seed within 255 tries, which is rare");
   }
   
-
+ 
+ 
   // Constants for BFS cuckoo path search:
   // The visited list must be maintained for all but the last level of search
   // in order to trace back the path. The BFS search has two roots
@@ -607,16 +555,15 @@ public:
   using ParrotCommon<K, V, VL>::h;
   using ParrotCommon<K, V, VL>::digestH;
   using ParrotCommon<K, V, VL>::Sentinel;
-  using ParrotCommon<K, V, VL>::Sentinel32;
-  using ParrotCommon<K, V, VL>::Sentinel32Mask;
-
+  
   typedef Ludo_PathEntry<K> PathEntry;
   
   struct LinkNode {
     std::array<K, kSlotPerLinknode> keys;
     std::array<V, kSlotPerLinknode> values;
-    // uint8_t fingerprints[kSlotPerLinknode];
+    //uint8_t fingerprints[kSlotPerLinknode];
     LinkNode* next = nullptr;
+
     LinkNode() {
       std::fill(keys.begin(), keys.end(), Sentinel);
     }
@@ -657,14 +604,13 @@ public:
       previous->next->insert(key, value, tape);
       return;
     }
+
   };
   
   struct Bucket { // only as parameters and return values for easy access. the storage is compact.
     uint8_t seed;
-    K bar = Sentinel;
     V values[kSlotsPerBucket];
-    uint32_t offsett = Sentinel32;
-    // /LinkNode *head = nullptr;
+    LinkNode *head = nullptr;
 
     bool operator==(const Bucket &other) const {
       if (seed != other.seed) return false;
@@ -678,17 +624,30 @@ public:
       return !(*this == other);
     }
 
-    //uint64_t localSize() {
-    //  return 0;
-    //}
+    inline int insert(const K &k, const V &v, support::Tape<LinkNode>& tape) {
+      if (!head) {
+        LinkNode* phead = new LinkNode();
+        phead->next = head;
+        head = phead;
+      }
+      head->insert(k, v, tape);
+      return 0;
+    }
+
+    inline uint32_t localSize() {
+      uint32_t res = sizeof(Bucket);
+      for (LinkNode* phead = head; phead != nullptr; phead = phead->next) {
+        res += sizeof(LinkNode);
+      }
+      return res;
+    }
   };
+  //  static const uint8_t bucketLength = sizeof(Bucket);
   
   Model model;
-  uint32_t empty_num_bucket;
   std::vector<Bucket> buckets;
   std::vector<uint8_t> lock = vector<uint8_t>(8192, 0);
   std::unique_ptr<support::Tape<LinkNode>> tape;
-  std::vector<LinkNode*> overflow;
 
   ParrotDataPlane():tape(std::make_unique<support::Tape<LinkNode>>()) {}
   
@@ -697,100 +656,43 @@ public:
 
     num_buckets_ = cp.num_buckets_;
     resetMemory();
-
-    std::queue<uint32_t> q = emptyBuckets(cp);
-    std::cout << "empty num: " << q.size() << ", total num: " << num_buckets_ << std::endl;
-    uint64_t num_chain_node = 0;
+    
     for (uint32_t bktIdx = 0; bktIdx < num_buckets_; ++bktIdx) {
       const typename ParrotControlPlane<K, V, OverAlloc, Model, VL>::Bucket &cpBucket = cp.buckets_[bktIdx];
       
       Bucket &dpBucket = buckets[bktIdx];
+      
       dpBucket.seed = cpBucket.seed;
       memset(dpBucket.values, 0, kSlotsPerBucket * sizeof(V));
       
-      AbslFastHasherTuple<K> locateHash(cpBucket.seed);
+      const AbslFastHasherTuple<K> locateHash(cpBucket.seed);
       //uint nkeys(0);
       for (char slot = 0; slot < kSlotsPerBucket; ++slot) {
         if (cpBucket.occupiedMask & (1U << slot)) {
+          //nkeys ++;
           const K &k = cpBucket.keys[slot];
           dpBucket.values[locateHash(k) >> 6] = cpBucket.values[slot];
         }
       }
-      
-      uint32_t len_chain = 0;
-      for (auto phead = cpBucket.head; phead != nullptr; phead = phead->next, len_chain++) {
-      }
-      if (len_chain > 0 && (len_chain > q.size())){ //|| q.empty()) {
-        num_chain_node += len_chain;
-        //std::cout << "This is bucket: " << bktIdx << ", chain len: " << len_chain  <<", qlen: " << q.size()<< std::endl;
-        LinkNode* qhead = nullptr;
-        for (auto phead = cpBucket.head; phead != nullptr; phead = phead->next) {
-          LinkNode* dhead = new LinkNode();
-          std::vector<std::pair<K, V>> tmp_pairs;
-          for (char slot = 0; slot < kSlotPerLinknode; ++slot) {
-            if (phead->occupiedMask & (1U << slot)) {
-              tmp_pairs.push_back(make_pair(phead->keys[slot], phead->values[slot]));
-            }
-          }
-          std::sort(tmp_pairs.begin(), tmp_pairs.end());
-          for (size_t pair_idx = 0; pair_idx < tmp_pairs.size(); pair_idx ++) {
-            dhead->keys[pair_idx] = tmp_pairs[pair_idx].first;
-            dhead->values[pair_idx] = tmp_pairs[pair_idx].second;
-          }
-          dhead->next = qhead;
-          qhead = dhead;
-        }
-        overflow.push_back(qhead);
-        dpBucket.offsett = (1UL << 31) + overflow.size() - 1;      // flag + offsett in overflow
-        //std::cout << "current overflow size is: " << overflow.size() << std::endl;
-        //if (true) {
-        //  uint32_t res = 0;
-        //  for (auto phead = overflow.back(); phead != nullptr; phead = phead->next, res++) {
-        //  }
-        //  std::cout << "res length of this chain is: " << res << std::endl;
-        //}
 
-      } else {
-        uint32_t previous_offsett = Sentinel32;
-        for (auto phead = cpBucket.head; phead != nullptr; phead = phead->next) {
-          auto dpBktIdx = q.front();
-          q.pop();
-          Bucket &newDpBucket = buckets[dpBktIdx];
-          newDpBucket.seed = phead->seed;
-          newDpBucket.bar = phead->bar;
-          locateHash.setSeed(phead->seed);
-          for (char slot = 0; slot < kSlotPerLinknode; ++slot) {
-            if (phead->occupiedMask & (1U << slot)) {
-              const K &k = phead->keys[slot];
-              newDpBucket.values[locateHash(k) >> 6] = phead->values[slot];
-            }
-          }
-          newDpBucket.offsett = previous_offsett;
-          previous_offsett = dpBktIdx;
-        }
-        dpBucket.offsett = previous_offsett;
+      for (auto phead = cpBucket.head; phead != nullptr; phead = phead->next) {
+        LinkNode* dhead = new LinkNode();
+        //for (uint i = 0; i < kSlotsPerBucket; i++) {
+        //  dhead->values[i] = phead->values
+        //}
+        //for (uint i = 0; i < kSlotsPerBucket; i++) {
+        //  if (phead->keys[i] != Sentinel)
+        //    nkeys ++;
+        //}
+        std::copy(phead->keys.begin(), phead->keys.end(), dhead->keys.begin());
+        std::copy(phead->values.begin(), phead->values.end(), dhead->values.begin());
+        dhead->next = dpBucket.head;
+        dpBucket.head = dhead;
       }
+      //std::cout << "number of keys for parrot: " << nkeys << std::endl;
     }
-    std::cout << "The size of overflow is: " << overflow.size() << std::endl;
-    std::cout << "The number of overflow nodes: " << num_chain_node << std::endl;
-    std::cout << "construction finished in data plane" << std::endl;
-    uint64_t ppp = (num_chain_node*sizeof(LinkNode) + num_buckets_*sizeof(Bucket))/(1024*1024);
-    std::cout << "The final size: " << ppp << std::endl;
-    //throw std::runtime_error("stop .");
   }
  
-  std::queue<uint32_t> emptyBuckets(const ParrotControlPlane<K, V, OverAlloc, Model, VL> &cp) {
-    std::queue<uint32_t> q;
-    for (uint32_t bktIdx = 0; bktIdx < num_buckets_; ++bktIdx) {
-      const typename ParrotControlPlane<K, V, OverAlloc, Model, VL>::Bucket &cpBucket = cp.buckets_[bktIdx];
-      //uint nkeys(0);
-      if ((cpBucket.occupiedMask & (1U << kSlotPerLinknode - 1)) == 0U) {
-        q.push(bktIdx);
-      }
-    }
-    return q;
-  }
-
   ~ParrotDataPlane(){
   }
 
@@ -808,45 +710,34 @@ public:
   // Returns true if found.  Sets *out = value.
   inline int lookUp(const K &k, V &out) {
     //auto start = std::chrono::high_resolution_clock::now(); 
-    //std::cout << "k: " <<k;
     uint32_t bucketId = model(k) % num_buckets_;
     //auto stop = std::chrono::high_resolution_clock::now(); 
     //auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start); 
     //std::cout<< std::endl << "Model latency is: "<< duration.count() << " nanoseconds" << std::endl;
-    Bucket &bucket = buckets[bucketId];
-    out = bucket.values[AbslFastHasherTuple<K>(bucket.seed)(k) >> 6];
-    uint32_t offsett = bucket.offsett;
-    while (offsett != Sentinel32) {
-      if (offsett & (1UL << 31)) {
-        //auto ppt = offsett & ((1UL << 31) - 1);
-        //std::cout << "offsett is: " << ppt << std::endl;
-        FindInLinkNode(k, out, offsett & Sentinel32Mask);
-        return 1;
-      } else {
-        Bucket &bucket = buckets[offsett];
-        if (bucket.bar > k) 
-          return 1;
-        out = bucket.values[AbslFastHasherTuple<K>(bucket.seed)(k) >> 6];
-        offsett = bucket.offsett;
-      }
-    }
-    return 0;
-  }
 
-  int inline FindInLinkNode (const K &k, V &out, uint32_t linkIdx) {
-    for (auto phead = overflow[linkIdx]; phead != nullptr; phead = phead->next) {
-      for (size_t i = 0; i < kSlotPerLinknode; i++) {
-        if (phead->keys[i] > k) {
-          return 1;
-        } else if (phead->keys[i] == k) {
-          out = phead->values[i];
-          return 0;
+    Bucket &bucket = buckets[bucketId];
+
+    out = bucket.values[AbslFastHasherTuple<K>(bucket.seed)(k) >> 6];
+    if (bucket.head == nullptr) {
+      return 0;
+    } else {
+      //std::cout << "new bucket --------------" << std::endl;
+      for (auto phead = bucket.head; phead != nullptr; phead = phead->next) {
+        //std::cout << "new slot chunk ----" << std::endl;
+        for (size_t i = 0; i < kSlotPerLinknode; i++) {
+          //std::cout << phead->keys[i] << std::endl;
+          if (phead->keys[i] > k) {
+            return 1;
+          } else if (phead->keys[i] == k) {
+            out = phead->values[i];
+            return 0;
+          }
         }
       }
     }
-    return 0;
+    return 1;  // not fail, just waste time
   }
-  /*
+  
   inline int insert(const K &k, const V &v) {
     uint32_t bucketId = model(k) % num_buckets_;
     Bucket &bucket = buckets[bucketId];
@@ -905,27 +796,18 @@ public:
     //std::cout << "idx: " << idx << std::endl;
     return res;
   }
-  */
-  inline uint64_t getMemoryCost() {
-    uint64_t res(0);
-    uint64_t num_link_node(0);
-    std::cout << "overflow size: " << overflow.size() << std::endl;
-    for (uint32_t bktIdx = 0; bktIdx < buckets.size(); bktIdx ++) {
-      res += sizeof(Bucket);
-      Bucket &bucket = buckets[bktIdx];
-      if ((bucket.offsett & (1UL << 31)) && (bucket.offsett != Sentinel32)) {
-        uint32_t linkIdx = bucket.offsett & ((1UL << 31) - 1);
-        for (auto &phead = overflow[linkIdx]; phead != nullptr; phead = phead->next) {
-          num_link_node ++;
-          res += sizeof(LinkNode);
-        }
-      }
+
+  inline uint32_t getMemoryCost() {
+    uint32_t res(0);
+    for (uint32_t i = 0; i < buckets.size(); i++) {
+      res += buckets[i].localSize();
     }
     res += model.byte_size();
-    std::cout << "link node number is: " << num_link_node << std::endl;
     return res;
   }
+
 };
+
 
 }
 
